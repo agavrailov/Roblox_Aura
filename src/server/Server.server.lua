abb002d7fe93a061a7b1eff5af401778e248fd1a
Anton Gavrailov <agavrailov@gmail.com>
2025-11-08 00:52:35 +0200

    fix: Correct path to PlayerData module in server script

--- src/server/Server.server.lua
+++ src/server/Server.server.lua
@@ -2,7 +2,7 @@
 -- Main server script for Aura Collector Simulator
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local PlayerData = require(ReplicatedStorage.shared.PlayerData)
+local PlayerData = require(ReplicatedStorage.PlayerData)
 
 print("Aura Collector Simulator Server Script Loaded")
 
